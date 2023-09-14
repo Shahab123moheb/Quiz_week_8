160e418b13eeb0b08de410b7a74822dcf7de5e23
Shahab Moheb <shahab.moheb@griffithuni.edu.au>
2023-09-14 11:52:25 +1000

    code to load the data from the csv file and show them in a figure

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+# Load the data from the CSV file
+data = pd.read_csv('climate.csv')
 
+# Extract columns into lists
+years = data['Year'].tolist()
+co2 = data['CO2'].tolist()
+temp = data['Temperature'].tolist()
+
+# Plotting code
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
-
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
