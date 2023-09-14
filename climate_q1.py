3ad9970a155855e5a6956e75086fe4b56f20c1dc
Shahab Moheb <shahab.moheb@griffithuni.edu.au>
2023-09-14 11:25:09 +1000

    Code added to fetch data

--- climate_q1.py
+++ climate_q1.py
@@ -1,9 +1,26 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
+
+# Connect to the database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+# Lists to hold the data
 years = []
 co2 = []
 temp = []
 
+# Fetch the data from the database
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData ORDER BY Year ASC")
+for row in cursor.fetchall():
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+# Close the database connection
+conn.close()
+
+# Plotting code (already in the file)
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
